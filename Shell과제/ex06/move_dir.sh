b2b313e3a21c7a4c09505a5b56292dcba7c02ad8
jinsoo <jinsoo@DESKTOP-GBJDINL>
2023-03-19 19:28:21 +0900

    Docs: Shell과제

--- Shell과제/ex06/move_dir.sh
+++ Shell과제/ex06/move_dir.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-touch test.txt
 
-mv test.txt ./newdir
+mv test.txt newdir
--- Shell과제/ex07/find_file.sh
+++ Shell과제/ex07/find_file.sh
@@ -3,4 +3,4 @@
 touch test1.sh
 touch test2.sh
 
-find /home/jinsoo/ex07/ -type f -name "*.sh"
+find /home/jinsoo/LikeLion-Common-Subject/Shell과제/ex07 -type f -name "*.sh"
