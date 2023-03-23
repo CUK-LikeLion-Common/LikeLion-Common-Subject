5cf3ed03412ea29ddbd12139ad81e29b862cecd6
hyunji kim <111987574+kimhyunji24@users.noreply.github.com>
2023-03-23 19:52:54 +0900

    Delete ex09 directory

--- ex09/myscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "scriptName:"$0
-Absolute_dir=$(cd "$(dirname "$0")" && pwd)
-echo "Absolute Path : "$Absolute_dir
-Relative_dir="$(command dirname -- "${0}")"
-echo "Relative Path : "$Relative_dir
